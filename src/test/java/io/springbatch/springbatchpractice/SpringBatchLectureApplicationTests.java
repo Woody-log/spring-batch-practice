aeae7a5d5841622354a0dde4a9f8571468cec14a
서윤철 <woody@29cm.co.kr>
2022-02-28 00:10:36 +0900

    Initial commit

---
+++ src/test/java/io/springbatch/springbatchpractice/SpringBatchLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbatch.springbatchpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchLectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
